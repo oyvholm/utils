078b164a30cc473b0395988e65f0743dbddf39fa
Øyvind A. Holm <sunny@sunbase.org>
2003-08-03 02:02:43 +0200

    Småfancy basis for nye Perl-script.
    git-svn-id: svn+ssh://svnhost/utils@1273 ddca7c6a-6bd8-0310-abd8-a0f6a7c394c1

---
+++ import/std/trunk/perl/std.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl -w
+
+# ╔══════════════════════════════════════════════════════════════
+# ║ $Id: std.pl,v 1.1 2003/08/03 00:02:43 sunny Exp $
+# ║ [Description]
+# ║ Character set: UTF-8
+# ║ License: GNU General Public License
+# ║ ©opyleft 2003 Øyvind A. Holm <sunny@sunbase.org>
+# ╚══════════════════════════════════════════════════════════════
+
+use strict;
+
+
+__END__
+
+# vim: set fileencoding=UTF-8 syntax=perl foldmethod=marker foldlevel=0 :
+
+# End of file $Id: std.pl,v 1.1 2003/08/03 00:02:43 sunny Exp $
