5fdf80020a42c60ca989708cc8da7e2daefcd8e9
Øyvind A. Holm <sunny@sunbase.org>
2003-09-24 11:54:17 +0200

    Jada, det var så tøfft atte med den rammeheaderen, men jeg setter det tilbake til mer standard stil. Det var litt overkill. Og jeg har lagt til $| = 1 også.
    git-svn-id: svn+ssh://svnhost/utils@1275 ddca7c6a-6bd8-0310-abd8-a0f6a7c394c1

--- import/std/trunk/perl/std.pl
+++ import/std/trunk/perl/std.pl
@@ -1,17 +1,20 @@
 #!/usr/bin/perl -w
 
-# ╔══════════════════════════════════════════════════════════════
-# ║ $Id: std.pl,v 1.2 2003/08/29 19:24:52 sunny Exp $
-# ║ [Description]
-# ║ Character set: UTF-8
-# ║ License: GNU General Public License
-# ║ ©opyleft 2003 Øyvind A. Holm <sunny@sunbase.org>
-# ╚══════════════════════════════════════════════════════════════
+#===============================================================
+# $Id: std.pl,v 1.3 2003/09/24 09:54:17 sunny Exp $
+# [Description]
+# Character set: UTF-8
+# License: GNU General Public License
+# ©opyleft 2003 Øyvind A. Holm <sunny@sunbase.org>
+#===============================================================
 
 use strict;
+
+$| = 1;
+
 
 
 __END__
 
 # vim: set fileencoding=UTF-8 filetype=perl foldmethod=marker foldlevel=0 :
-# End of file $Id: std.pl,v 1.2 2003/08/29 19:24:52 sunny Exp $
+# End of file $Id: std.pl,v 1.3 2003/09/24 09:54:17 sunny Exp $
