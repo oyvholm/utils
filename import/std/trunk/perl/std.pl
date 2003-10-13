3365a19bff9fb3e3afec9af257f52f2c1a937a49
Øyvind A. Holm <sunny@sunbase.org>
2003-10-13 07:29:21 +0200

    La inn ei tom linje i headeren.
    git-svn-id: svn+ssh://svnhost/utils@1276 ddca7c6a-6bd8-0310-abd8-a0f6a7c394c1

--- import/std/trunk/perl/std.pl
+++ import/std/trunk/perl/std.pl
@@ -1,8 +1,9 @@
 #!/usr/bin/perl -w
 
 #===============================================================
-# $Id: std.pl,v 1.3 2003/09/24 09:54:17 sunny Exp $
+# $Id: std.pl,v 1.4 2003/10/13 05:29:21 sunny Exp $
 # [Description]
+#
 # Character set: UTF-8
 # License: GNU General Public License
 # ©opyleft 2003 Øyvind A. Holm <sunny@sunbase.org>
@@ -17,4 +18,4 @@ $| = 1;
 __END__
 
 # vim: set fileencoding=UTF-8 filetype=perl foldmethod=marker foldlevel=0 :
-# End of file $Id: std.pl,v 1.3 2003/09/24 09:54:17 sunny Exp $
+# End of file $Id: std.pl,v 1.4 2003/10/13 05:29:21 sunny Exp $
