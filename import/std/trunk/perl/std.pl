267c0c1b52fe604420d88ed3ebba36ea6f489004
Øyvind A. Holm <sunny@sunbase.org>
2004-01-27 13:44:25 +0100

    Forandra copyrightåret til 2004.
    git-svn-id: svn+ssh://svnhost/utils@1277 ddca7c6a-6bd8-0310-abd8-a0f6a7c394c1

--- import/std/trunk/perl/std.pl
+++ import/std/trunk/perl/std.pl
@@ -1,12 +1,12 @@
 #!/usr/bin/perl -w
 
 #===============================================================
-# $Id: std.pl,v 1.4 2003/10/13 05:29:21 sunny Exp $
+# $Id: std.pl,v 1.5 2004/01/27 12:44:25 sunny Exp $
 # [Description]
 #
 # Character set: UTF-8
 # License: GNU General Public License
-# ©opyleft 2003 Øyvind A. Holm <sunny@sunbase.org>
+# ©opyleft 2004 Øyvind A. Holm <sunny@sunbase.org>
 #===============================================================
 
 use strict;
@@ -18,4 +18,4 @@ $| = 1;
 __END__
 
 # vim: set fileencoding=UTF-8 filetype=perl foldmethod=marker foldlevel=0 :
-# End of file $Id: std.pl,v 1.4 2003/10/13 05:29:21 sunny Exp $
+# End of file $Id: std.pl,v 1.5 2004/01/27 12:44:25 sunny Exp $
