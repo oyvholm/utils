f0e64a0e4923b478942052361faaaa686e419fc2
Øyvind A. Holm <sunny@sunbase.org>
2004-05-28 01:39:28 +0200

    Har lagt inn Getopt::Std og usage().
    git-svn-id: svn+ssh://svnhost/utils@1279 ddca7c6a-6bd8-0310-abd8-a0f6a7c394c1

--- import/std/trunk/perl/std.pl
+++ import/std/trunk/perl/std.pl
@@ -13,7 +13,25 @@ use strict;
 
 $| = 1;
 
+use Getopt::Std;
+our ($opt_d, $opt_h, $opt_i, $opt_s, $opt_v) = ("", 0, 0, 0, 0);
+getopts('h') || die("Option error. Use -h fopr help.");
 
+$opt_h && usage(0);
+
+
+
+sub usage {
+	# Send the help message to stdout {{{
+	my $Retval = shift;
+	print(<<END);
+
+Usage:
+
+END
+	exit($Retval);
+	# }}}
+}
 
 __END__
 
