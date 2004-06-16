44ea4ad97f04f02d792a9822b188a885ac7f1a87
Øyvind A. Holm <sunny@sunbase.org>
2004-06-16 14:59:36 +0200

    * perl/std.pl   La til en POD-seksjon som er henta fra cvse r819.
    git-svn-id: svn+ssh://svnhost/utils@1280 ddca7c6a-6bd8-0310-abd8-a0f6a7c394c1

--- import/std/trunk/perl/std.pl
+++ import/std/trunk/perl/std.pl
@@ -35,5 +35,63 @@ END
 
 __END__
 
+# Plain Old Documentation (POD) {{{
+
+=pod
+
+=head1 NAME
+
+
+
+=head1 REVISION
+
+$Id$
+
+=head1 SYNOPSIS
+
+
+
+=head1 DESCRIPTION
+
+
+
+=head1 OPTIONS
+
+=over 4
+
+=item B<-h>
+
+Print a brief help summary.
+
+=back
+
+=head1 BUGS
+
+
+
+=head1 AUTHOR
+
+Made by Øyvind A. Holm S<E<lt>sunny _AT_ sunbase.orgE<gt>>.
+
+=head1 COPYRIGHT
+
+Copyleft © Øyvind A. Holm &lt;sunny@sunbase.org&gt;
+This is free software; see the file F<COPYING> for legalese stuff.
+
+=head1 LICENCE
+
+This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+=head1 SEE ALSO
+
+=cut
+
+# }}}
+
 # vim: set fileencoding=UTF-8 filetype=perl foldmethod=marker foldlevel=0 :
 # End of file $Id$
