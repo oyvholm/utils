007f5a1ae33c3ead99d1123240b308993b48a3b7
Øyvind A. Holm <sunny@sunbase.org>
2006-10-31 18:06:35 +0100

    Flytta index-kjøret til en egen fil.
    * /trunk/gps/div/postgres/create_table.sql
      Herfra...
    
    * /trunk/gps/div/postgres/create_index.sql
      Til denne fila som er ny.
    
    
    git-svn-id: svn+ssh://svnhost/utils@1929 ddca7c6a-6bd8-0310-abd8-a0f6a7c394c1

---
+++ import/gps-postgres/trunk/gps/div/postgres/create_index.sql
@@ -0,0 +1,6 @@
+-- $Id$
+
+CREATE INDEX date_idx on logg(date);
+CREATE INDEX lon_idx on logg(lon);
+CREATE INDEX lat_idx on logg(lat);
+CREATE INDEX alt_idx on logg(alt);
--- import/gps-postgres/trunk/gps/div/postgres/create_table.sql
+++ import/gps-postgres/trunk/gps/div/postgres/create_table.sql
@@ -6,8 +6,3 @@ CREATE TABLE logg (
     lat numeric,
     alt numeric
 );
-
-CREATE INDEX date_idx on logg(date);
-CREATE INDEX lon_idx on logg(lon);
-CREATE INDEX lat_idx on logg(lat);
-CREATE INDEX alt_idx on logg(alt);
