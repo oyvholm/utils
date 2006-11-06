d62a4828219fbb004b3fbc95af6ecad94fc656d1
Øyvind A. Holm <sunny@sunbase.org>
2006-11-06 23:50:22 +0100

    * /trunk/gps/div/postgres/create_views.sql   Har laget en dunge med views. Kanskje småtøfft.
    git-svn-id: svn+ssh://svnhost/utils@1933 ddca7c6a-6bd8-0310-abd8-a0f6a7c394c1

---
+++ import/gps-postgres/trunk/gps/div/postgres/create_views.sql
@@ -0,0 +1,122 @@
+ -- $Id$
+
+CREATE OR REPLACE VIEW siste_aar
+    AS SELECT * from (
+        SELECT DISTINCT ON (
+            sted, date_trunc('week', date)
+        ) *
+        FROM logg
+        WHERE date > now()+interval '1 year ago'
+    ) as s
+    ORDER BY date;
+
+CREATE OR REPLACE VIEW siste_halvaar
+    AS SELECT * from (
+        SELECT DISTINCT ON (
+            sted, date_trunc('week', date)
+        ) *
+        FROM logg
+        WHERE date > now()+interval '0.5 year ago'
+    ) as s
+    ORDER BY date;
+
+CREATE OR REPLACE VIEW siste_maaned
+    AS SELECT * from (
+        SELECT DISTINCT ON (
+            sted, date_trunc('day', date)
+        ) *
+        FROM logg
+        WHERE date > now()+interval '1 month ago'
+    ) as s
+    ORDER BY s.date;
+
+CREATE OR REPLACE VIEW siste_uke
+    AS SELECT * from (
+        SELECT DISTINCT ON (
+            sted, date_trunc('hour', date)
+        ) *
+        FROM logg
+        WHERE date > now()+interval '1 week ago'
+    ) as s
+    ORDER BY date;
+
+CREATE OR REPLACE VIEW siste_dogn
+    AS SELECT * from (
+        SELECT DISTINCT ON (
+            sted, date_trunc('minute', date)
+        ) *
+        FROM logg
+        WHERE date > now()+interval '1 day ago'
+    ) as s
+    ORDER BY date;
+
+/*** Fjernest ***/
+
+CREATE OR REPLACE VIEW fjernest
+    AS SELECT * from (
+        SELECT DISTINCT ON (
+            sted, date_trunc('week', date)
+        ) *
+        FROM logg
+    ) as s
+    ORDER BY avst DESC;
+
+CREATE OR REPLACE VIEW fjernest_siste_aar
+    AS SELECT * from (
+        SELECT DISTINCT ON (
+            sted, date_trunc('week', date)
+        ) *
+        FROM logg
+        WHERE date > now()+interval '1 year ago') as s
+    ORDER BY avst DESC;
+
+CREATE OR REPLACE VIEW fjernest_siste_halvaar
+    AS SELECT * from (
+        SELECT DISTINCT ON (
+            sted, date_trunc('week', date)
+        ) *
+        FROM logg
+        WHERE date > now()+interval '0.5 year ago'
+    ) as s
+    ORDER BY avst DESC;
+
+CREATE OR REPLACE VIEW fjernest_siste_maaned
+    AS SELECT * from (
+        SELECT DISTINCT ON (
+            sted, date_trunc('day', date)
+        ) *
+        FROM logg
+        WHERE date > now()+interval '1 month ago'
+    ) as s
+    ORDER BY avst DESC;
+
+CREATE OR REPLACE VIEW fjernest_siste_uke
+    AS SELECT * from (
+        SELECT DISTINCT ON (
+            sted, date_trunc('hour', date)
+        ) *
+        FROM logg
+        WHERE date > now()+interval '1 week ago'
+    ) as s
+    ORDER BY avst DESC;
+
+CREATE OR REPLACE VIEW fjernest_siste_dogn
+    AS SELECT * from (
+        SELECT DISTINCT ON (
+            sted, date_trunc('minute', date)
+        ) *
+        FROM logg
+        WHERE date > now()+interval '1 day ago'
+    ) as s
+    ORDER BY avst DESC;
+
+/*** Intervaller ***/
+
+CREATE OR REPLACE VIEW minutt
+    AS SELECT * from (
+        SELECT DISTINCT ON (
+            sted, date_trunc('minute', date)
+        ) *
+        FROM logg
+    ) as s
+    ORDER BY date DESC;
