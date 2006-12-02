8a98fe857e101318eea2485d497d621dcf7ee22a
Øyvind A. Holm <sunny@sunbase.org>
2006-12-02 14:51:42 +0100

    * /trunk/gps/div/postgres/create_funcs.sql   Hurrahopp, nå har jeg lagd mine første fornuftige funksjoner.   closest() og plass(). Funker rimelig greit.
    git-svn-id: svn+ssh://svnhost/utils@1959 ddca7c6a-6bd8-0310-abd8-a0f6a7c394c1

--- import/gps-postgres/trunk/gps/div/postgres/create_funcs.sql
+++ import/gps-postgres/trunk/gps/div/postgres/create_funcs.sql
@@ -1,11 +1,31 @@
 -- $Id$
 
-CREATE OR REPLACE FUNCTION closest(numeric, numeric) RETURNS text AS $$
-    SELECT name::text
-        FROM wayp
-        WHERE point($1, $2) = (
-            SELECT koor
-                FROM wayp
-                WHERE point($1, $2) <-> koor = min(point($1, $2) <-> koor)
-        );
+DROP FUNCTION closest(point);
+CREATE OR REPLACE FUNCTION closest(point) RETURNS text
+AS $$
+SELECT wp_name || ' (' || round(avs::numeric, 5) || ')' FROM (
+        SELECT
+            wp_name,
+            ($1 <-> wp_koor)
+            AS avs
+            FROM wayp
+            WHERE ($1 <-> wp_koor) < 0.05
+            ORDER BY avs
+            LIMIT 1
+    ) AS s;
+$$ LANGUAGE SQL;
+
+DROP FUNCTION plass(point);
+CREATE OR REPLACE FUNCTION plass(point) RETURNS text
+AS $$
+SELECT wp_name FROM (
+        SELECT
+            wp_name,
+            ($1 <-> wp_koor)
+            AS avs
+            FROM wayp
+            WHERE ($1 <-> wp_koor) < 0.0002
+            ORDER BY avs
+            LIMIT 1
+    ) AS s;
 $$ LANGUAGE SQL;
