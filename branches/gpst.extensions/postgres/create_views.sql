d15b144a47bf3d93c9975c3263cd615a83ff9b4e
Øyvind A. Holm <sunny@sunbase.org>
2007-03-11 02:11:52 +0100

    Merga inn alt fra trunk opp til r2055, det vil si alt som er.
    * /branches/gpst.extensions/Patch
    * /branches/gpst.extensions/Patch/gpst-replace
    * /branches/gpst.extensions/Patch/gpst-u-integrate
    * /branches/gpst.extensions/Patch/gpst-xmlsimple
    * /branches/gpst.extensions/Patch/mayko-comments.patch
    * /branches/gpst.extensions/Patch/xgraph-fix_plus_more.patch
    * /branches/gpst.extensions/doc/mayko.txt
    * /branches/gpst.extensions/gpslist
    * /branches/gpst.extensions/postgres/distupdate.sql
    * /branches/gpst.extensions/tests/different_dateformats.gpsml
      Nytt.
    
    * /branches/gpst.extensions/Utv/
    * /branches/gpst.extensions/xgraph-fix_plus_more.patch
      Sletta.
    
    * /branches/gpst.extensions/gpst
    * /branches/gpst.extensions/postgres/create_table.sql
    * /branches/gpst.extensions/postgres/create_views.sql
    * /branches/gpst.extensions/postgres/update_things.sql
    * /branches/gpst.extensions/tests/multitrack-pause.gpsml
    * /branches/gpst.extensions/tests/multitrack-pause.gpx
    * /branches/gpst.extensions/tests/run-tests.pl
      Modifisert.
    
    * /branches/gpst.extensions/tests/comments.mayko
    * /branches/gpst.extensions/tests/compact.gpx
    * /branches/gpst.extensions/tests/multitrack.gpx
    * /branches/gpst.extensions/tests/no_signal.mayko
    * /branches/gpst.extensions/tests/pause.gpx
      Egenskapsforandringer.
    
    * /branches/gpst.extensions/tests/chronology-error.gpsml
    * /branches/gpst.extensions/tests/missing.gpsml
    * /branches/gpst.extensions/tests/one_ele.dos.gpsml
      Her var det konflikter i Id-kjøret, samme opplegget som i r2063.
    
    * /branches/gpst.extensions/
      Oppdatering av mergesvn-egenskapen.
    
    
    git-svn-id: svn+ssh://svnhost/utils@2069 ddca7c6a-6bd8-0310-abd8-a0f6a7c394c1

--- branches/gpst.extensions/postgres/create_views.sql
+++ branches/gpst.extensions/postgres/create_views.sql
@@ -94,8 +94,20 @@ CREATE OR REPLACE VIEW minutt
 
 /*** Formater ***/
 
-CREATE OR REPLACE VIEW gpx
-    AS SELECT * FROM logg LIMIT 1;
+CREATE OR REPLACE VIEW gpx AS
+    SELECT '<trkpt lat="' || coor[0] || '" lon="' || coor[1] || '"> ' ||
+        '<ele>' || ele || '</ele> ' ||
+        '<time>' || date || '</time> ' ||
+    '</trkpt>'
+    AS gpx,
+    date, coor, ele, sted, dist, description
+    FROM logg;
+
+CREATE OR REPLACE VIEW gpst AS
+    SELECT date, coor, ele, sted, dist, avst,
+    '<tp> <time>' || date at time zone 'UTC' || 'Z' || '</time> <lat>' || coor[0] || '</lat> <lon>' || coor[1] || '</lon> </tp>'
+    AS gpst
+    FROM logg;
 
 -- Lister ut events sammen med loggen.
 CREATE OR REPLACE VIEW ev AS
--- branches/gpst.extensions/postgres/update_things.sql
+++ branches/gpst.extensions/postgres/update_things.sql
@@ -97,35 +97,4 @@ SELECT count(*)
     AS "Antall i events etter rensking"
     FROM events;
 
-BEGIN ISOLATION LEVEL SERIALIZABLE;
-    \echo
-    \echo ================ Sett avstanden hjemmefra ================
-
-    UPDATE logg SET avst = '(60.42543,5.29959)'::point <-> coor
-        WHERE date > (
-            SELECT laststed FROM stat
-                WHERE lastupdate IS NOT NULL
-                ORDER BY lastupdate DESC LIMIT 1
-        );
-
-    \echo
-    \echo ================ Oppdater sted og dist ================
-
-    UPDATE logg SET sted = clname(coor)
-        WHERE date > (
-            SELECT laststed FROM stat
-                WHERE lastupdate IS NOT NULL
-                ORDER BY lastupdate DESC LIMIT 1
-        );
-    UPDATE logg SET dist = cldist(coor)
-        WHERE date > (
-            SELECT laststed FROM stat
-                WHERE lastupdate IS NOT NULL
-                ORDER BY lastupdate DESC LIMIT 1
-        );
-
-    INSERT INTO stat (lastupdate, laststed) VALUES (
-        now(),
-        (SELECT max(date) FROM logg)
-    );
-COMMIT;
+\i distupdate.sql
