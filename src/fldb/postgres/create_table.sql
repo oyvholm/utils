7d98facae1700f3bb604f560ecc45f23c8be9a3a
Øyvind A. Holm <sunny@sunbase.org>
2008-02-27 00:23:03 +0100

    * /trunk/src/fldb/postgres/   Ny.
    * /trunk/src/fldb/postgres/create_table.sql
      Ny. Lager "files" og "other".
    
    
    git-svn-id: svn+ssh://svnhost/utils@2198 ddca7c6a-6bd8-0310-abd8-a0f6a7c394c1

---
+++ src/fldb/postgres/create_table.sql
@@ -0,0 +1,41 @@
+-- $Id$
+
+CREATE TABLE files (
+    sha1 varchar(40),
+    md5 varchar(32),
+    crc32 varchar(32),
+    size bigint,
+    filename varchar,
+    mtime timestamptz,
+    ctime timestamptz,
+    path varchar,
+    device varchar,
+    hostname varchar,
+    uid varchar,
+    gid varchar,
+    perm varchar,
+    lastver varchar(40),
+    nextver varchar(40),
+    descr varchar
+);
+
+CREATE TABLE other (
+    kind varchar,
+    sha1 varchar(40),
+    md5 varchar(32),
+    crc32 varchar(32),
+    size bigint,
+    filename varchar,
+    symlink varchar,
+    mtime timestamptz,
+    ctime timestamptz,
+    path varchar,
+    device varchar,
+    hostname varchar,
+    uid varchar,
+    gid varchar,
+    perm varchar,
+    lastver varchar(40),
+    nextver varchar(40),
+    descr varchar
+);
