4a50db70a9fcb76b347beb928c03226a760dfc9c
Øyvind A. Holm <sunny@sunbase.org>
2008-05-22 22:42:34 +0200

    Oppretter moduler i fldb.
    * /trunk/src/fldb/fldb
      (checksum): Flytta til FLDBsum.pm .
      (valid_utf8, latin1_to_utf8, wideahchar): Flytta til FLDButf.pm .
    
    * /trunk/src/fldb/FLDBsum.pm
      Ny. Måtte legge til et ekstra parameter til cheksum() som bestemmer om 
      CRC32 skal regnes ut.
    
    * /trunk/src/fldb/FLDButf.pm
      Ny.
    
    
    git-svn-id: svn+ssh://svnhost/utils@2237 ddca7c6a-6bd8-0310-abd8-a0f6a7c394c1

---
+++ src/fldb/FLDBsum.pm
@@ -0,0 +1,63 @@
+package FLDBsum;
+
+#=======================================================================
+# $Id$
+#
+# Character set: UTF-8
+# ©opyleft 2002– Øyvind A. Holm <sunny@sunbase.org>
+# License: GNU General Public License, see end of file for legal stuff.
+#=======================================================================
+
+use strict;
+use warnings;
+use Digest::MD5;
+use Digest::SHA1;
+use Digest::CRC;
+use Time::HiRes qw{ gettimeofday };
+
+BEGIN {
+    use Exporter ();
+    our ($VERSION, @ISA, @EXPORT, @EXPORT_OK, %EXPORT_TAGS);
+
+    my $rcs_id = '$Id$';
+    push(@main::version_array, $rcs_id);
+    $VERSION = ($rcs_id =~ / (\d+) /, $1);
+
+    @ISA = qw(Exporter);
+    @EXPORT = qw(&checksum);
+    %EXPORT_TAGS = ();
+}
+our @EXPORT_OK;
+
+sub checksum {
+    # {{{
+    my ($Filename, $use_crc32) = @_;
+    my $Retval = "";
+    my %Sum = ();
+    my $starttime = gettimeofday;
+    local *FP;
+
+    D("checksum(\"$Filename\"");
+    if (open(FP, "<", "$Filename")) {
+        my $sha1 = Digest::SHA1->new;
+        my $md5 = Digest::MD5->new;
+        my $crc32 = Digest::CRC->new(type => "crc32");
+        while (my $Curr = <FP>) {
+            $sha1->add($Curr);
+            $md5->add($Curr);
+            $crc32->add($Curr) if $use_crc32;
+        }
+        $Sum{'sha1'} = $sha1->hexdigest;
+        $Sum{'md5'} = $md5->hexdigest;
+        $use_crc32 && ($Sum{'crc32'} = sprintf("%08x", $crc32->digest));
+    } else {
+        # warn("$Filename: Cannot read file: $!\n");
+    }
+    my $endtime = gettimeofday;
+    $Sum{'calctime'} = $endtime-$starttime;
+    D("checksum() returnerer " . scalar(%Sum) . " elementer");
+    return(%Sum);
+    # }}}
+} # checksum()
+
+1;
