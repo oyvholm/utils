b2de842fa9b89bd16933926c52a8d4419246b7ff
Øyvind A. Holm <sunny@sunbase.org>
2009-01-25 22:16:11 +0100

    * /branches/gpst.postgis/postgres/init.sh   La inn defineringa av PostGIS-ting som forsvant i r2817.
    61eed242-eb25-11dd-8b2b-000475e441b9
    
    
    git-svn-id: svn+ssh://svnhost/utils@2821 ddca7c6a-6bd8-0310-abd8-a0f6a7c394c1

--- branches/gpst.postgis/postgres/init.sh
+++ branches/gpst.postgis/postgres/init.sh
@@ -4,4 +4,6 @@
 
 createdb gps
 psql -d gps -c "CREATE LANGUAGE plpgsql;"
+psql -d gps -f /usr/share/postgresql-8.3-postgis/lwpostgis.sql
+psql -d gps -f /usr/share/postgresql-8.3-postgis/spatial_ref_sys.sql
 psql -d gps -f init.sql
