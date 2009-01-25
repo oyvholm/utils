1b5355a04f698f87ce70e9980a681f065cade2d0
Øyvind A. Holm <sunny@sunbase.org>
2009-01-25 22:02:14 +0100

    * /trunk/src/gpstools/postgres/init.sh   Fjerner alt som har med PostGIS å gjøre, det havner på en egen branch.   Det var mye rart som skjedde der, ser ut som hastigheta gikk dramatisk   ned når ting skulle oppdateres. Får se hva det går i.     Nå skal ting være bortimot som før, bare med den forskjellen at   addpoint(1) kan brukes istedenfor manuell kopiering.
    6f6f1e06-eb23-11dd-8b2b-000475e441b9
    
    
    git-svn-id: svn+ssh://svnhost/utils@2817 ddca7c6a-6bd8-0310-abd8-a0f6a7c394c1

--- src/gpstools/postgres/init.sh
+++ src/gpstools/postgres/init.sh
@@ -2,9 +2,6 @@
 
 # $Id$
 
-# createdb -T template_postgis gps
 createdb gps
 psql -d gps -c "CREATE LANGUAGE plpgsql;"
-psql -d gps -f /usr/share/postgresql-8.3-postgis/lwpostgis.sql
-psql -d gps -f /usr/share/postgresql-8.3-postgis/spatial_ref_sys.sql
 psql -d gps -f init.sql
