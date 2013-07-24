201fa90788fc1ecf2ad4f6d958c697334a3b415a
Øyvind A. Holm <sunny@sunbase.org>
2013-07-24 20:54:06 +0200

    line-exec.py works.
    7f02ca48-f492-11e2-81fc-a088b4ddef28

--- line-exec.py
+++ line-exec.py
@@ -1,10 +1,36 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""line-exec.py - STDdescDTS
+"""line-exec.py - Send all lines from stdin to specified programs
+
+Reads from stdin and sends every line to the program(s) specified on the 
+command line.
 
 File ID: 0a869b50-f45a-11e2-bb76-a088b4ddef28
 License: GNU General Public License version 3 or later.
 
 """
 
+import sys
+import subprocess
+
+def exec_line(line, args):
+    for cmd in args:
+        pipe = subprocess.Popen(cmd, stdin=subprocess.PIPE,
+                                stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        (stdout, stderr) = pipe.communicate(line)
+        sys.stdout.write(stdout)
+
+def main(args = None):
+    from optparse import OptionParser
+    parser = OptionParser()
+    (opt, args) = parser.parse_args()
+
+    fp = sys.__stdin__
+    line = fp.readline()
+    while line:
+        exec_line(line, args)
+        line = fp.readline()
+
+if __name__ == "__main__":
+    main()
