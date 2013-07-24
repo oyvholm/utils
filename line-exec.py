33e2a6f73732491d18e7f7f183c9ca7350fbb616
Øyvind A. Holm <sunny@sunbase.org>
2013-07-24 14:10:46 +0200

    $ std -l python line-exec.py
    1477dd9a-f45a-11e2-ae79-a088b4ddef28

---
+++ line-exec.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""line-exec.py - STDdescDTS
+
+File ID: 0a869b50-f45a-11e2-bb76-a088b4ddef28
+License: GNU General Public License version 3 or later.
+
+"""
+
