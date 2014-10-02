c9ceb3ac0a38d1ba60ea98048d142f2d4fb3df8b
Øyvind A. Holm <sunny@sunbase.org>
2014-10-03 00:31:06 +0200

    jsonfmt.py works, uses only stdin/stdout atm
    ed9b3f9c-4a83-11e4-814a-c80aa9e67bbd

--- jsonfmt.py
+++ jsonfmt.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-"""jsonfmt.py
+"""jsonfmt.py - JSON formatter
 
 File ID: 423dd854-4a64-11e4-97b1-c80aa9e67bbd
 License: GNU General Public License version 2 or later.
@@ -9,16 +9,33 @@ Author: Øyvind A. Holm <sunny@sunbase.org>
 
 """
 
+def format_json(text):
+    """Return formatted JSON"""
+    import json
+
+    return json.dumps(
+        json.loads(text),
+        ensure_ascii=False,
+        indent=4,
+        sort_keys=True,
+        )
+
 def main():
     import argparse
     import os
+    import sys
 
     progname = os.path.basename(__file__)
 
     parser = argparse.ArgumentParser(
-        description='',
+        description='JSON formatter',
         )
     args = parser.parse_args()
+
+    try:
+        print(format_json(''.join(sys.stdin.readlines())))
+    except ValueError:
+        sys.stderr.write("%s: Invalid JSON\n" % progname)
 
 if __name__ == "__main__":
     main()
