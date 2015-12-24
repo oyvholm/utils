90e7c6199fc21bf07d16e016f272767d61f0532b
Øyvind A. Holm <sunny@sunbase.org>
2015-12-24 18:55:30 +0100

    Update local sync info in Lib/std/book-cmark/synced.sql
    Syncs against this repository. If changes are made to, let's say
    Lib/std/perl, it's synced downstream to Lib/std/book-cmark/bin/stats,
    and all "clones" of book-cmark/ sync from there.
    
    f6d6fcb4-aa67-11e5-8c5a-fefdb24f8e10

--- Lib/std/book-cmark/synced.sql
+++ Lib/std/book-cmark/synced.sql
@@ -16,9 +16,9 @@ CREATE TABLE synced (
     CONSTRAINT synced_date_valid
       CHECK (date IS NULL OR datetime(date) IS NOT NULL)
 );
-INSERT INTO "synced" VALUES('bin/create-html','Lib/std/bash','7365eb760dd4bd581aeedc53fcc10cb1b8ba17ca','2015-12-13 09:20:30');
-INSERT INTO "synced" VALUES('bin/stats','Lib/std/perl','edf69ab8cacd6e39d48f3e5b842e89e64edd86a1','2015-12-12 07:06:46');
-INSERT INTO "synced" VALUES('index.md','Lib/std/markdown','edf69ab8cacd6e39d48f3e5b842e89e64edd86a1','2015-12-12 05:02:39');
+INSERT INTO "synced" VALUES('bin/create-html','Lib/std/book-cmark/bin/create-html','ffe510ea2fbb5baf8f7f5573d86e3168a9ce7df0','2015-12-24 17:11:25');
+INSERT INTO "synced" VALUES('bin/stats','Lib/std/book-cmark/bin/stats','ffe510ea2fbb5baf8f7f5573d86e3168a9ce7df0','2015-12-24 17:11:25');
+INSERT INTO "synced" VALUES('index.md','Lib/std/book-cmark/index.md','ffe510ea2fbb5baf8f7f5573d86e3168a9ce7df0','2015-12-24 17:11:25');
 CREATE TABLE todo (
   file TEXT
     CONSTRAINT todo_file_length
