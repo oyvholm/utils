7bedc5ab5ce376d2e97085c492c4231c03f4a20d
Øyvind A. Holm <sunny@sunbase.org>
2019-06-02 02:28:45 +0200

    selftest.c: Replace hardcoded year with `STDyearDTS`
    7493da58-84cd-11e9-81f0-4f45262dc9b5

--- Lib/std/c/src/selftest.c
+++ Lib/std/c/src/selftest.c
@@ -2,7 +2,7 @@
  * selftest.c
  * File ID: STDuuidDTS
  *
- * (C)opyleft 2017- Øyvind A. Holm <sunny@sunbase.org>
+ * (C)opyleft STDyearDTS- Øyvind A. Holm <sunny@sunbase.org>
  *
  * This program is free software; you can redistribute it and/or modify it 
  * under the terms of the GNU General Public License as published by the Free 
