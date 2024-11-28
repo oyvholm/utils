0cc42cb65f339be24f791e4ad0456e8b47a6ac6a
Øyvind A. Holm <sunny@sunbase.org>
2024-11-28 14:28:33 +0100

    selftest.c: Create diag() with related functions and tests
    c98a8bf4-ad8c-11ef-8749-83850402c3ce

--- Lib/std/c/src/selftest.c
+++ Lib/std/c/src/selftest.c
@@ -52,6 +52,195 @@ static int ok(const int i, const char *desc, ...)
 }
 
 /*
+ * diag_output_va() - Receives a printf-like string and returns an allocated 
+ * string, prefixed with "# " and all '\n' characters converted to "\n# ". 
+ * Returns NULL if anything fails or `format` is NULL.
+ */
+
+static char *diag_output_va(const char *format, va_list ap)
+{
+	const char *src;
+	char *buffer, *converted_buffer, *dst;
+	int needed;
+	size_t buffer_size = BUFSIZ, converted_size;
+	va_list ap_copy;
+
+	if (!format)
+		return NULL; /* gncov */
+
+	buffer = malloc(buffer_size);
+	if (!buffer)
+		return NULL; /* gncov */
+
+	va_copy(ap_copy, ap);
+	needed = vsnprintf(buffer, buffer_size, format, ap);
+
+	if ((size_t)needed >= buffer_size) {
+		free(buffer);
+		buffer_size = (size_t)needed + 1;
+		buffer = malloc(buffer_size);
+		if (!buffer)
+			return NULL; /* gncov */
+		vsnprintf(buffer, buffer_size, format, ap_copy);
+	}
+	va_end(ap_copy);
+
+	/* Prepare for worst case, every char is a newline. */
+	converted_size = strlen(buffer) * 3 + 1;
+	converted_buffer = malloc(converted_size);
+	if (!converted_buffer) {
+		free(buffer); /* gncov */
+		return NULL; /* gncov */
+	}
+
+	src = buffer;
+	dst = converted_buffer;
+	*dst++ = '#';
+	*dst++ = ' ';
+	while (*src) {
+		if (*src == '\n') {
+			*dst++ = '\n';
+			*dst++ = '#';
+			*dst++ = ' ';
+		} else {
+			*dst++ = *src;
+		}
+		src++;
+	}
+	*dst = '\0';
+	free(buffer);
+
+	return converted_buffer;
+}
+
+/*
+ * diag_output() - Frontend against diag_output_va(), used by the tests. 
+ * Returns number of failed tests.
+ */
+
+static char *diag_output(const char *format, ...)
+{
+	va_list ap;
+	char *result;
+
+	if (!format)
+		return NULL;
+
+	va_start(ap, format);
+	result = diag_output_va(format, ap);
+	va_end(ap);
+
+	return result;
+}
+
+/*
+ * diag() - Prints a diagnostic message prefixed with "# " to stdout. `printf` 
+ * sequences can be used. All `\n` characters are converted to "\n# ".
+ *
+ * A terminating `\n` is automatically added to the string. Returns 0 if 
+ * successful, or 1 if `format` is NULL.
+ */
+
+static int diag(const char *format, ...)
+{
+	va_list ap;
+	char *converted_buffer;
+
+	if (!format)
+		return 1;
+
+	va_start(ap, format);
+	converted_buffer = diag_output_va(format, ap);
+	va_end(ap);
+	if (!converted_buffer)
+		return ok(1, "%s(): diag_output_va() failed", /* gncov */
+		             __func__);
+	fprintf(stderr, "%s\n", converted_buffer);
+	fflush(stderr);
+	free(converted_buffer);
+
+	return 0;
+}
+
+/*
+ ******************
+ * Function tests *
+ ******************
+ */
+
+/*
+ * selftest functions
+ */
+
+/*
+ * test_diag_big() - Tests diag_output() with a string larger than BUFSIZ. 
+ * Returns the number of failed tests.
+ */
+
+static int test_diag_big(void)
+{
+	int r = 0;
+	size_t size;
+	char *p, *outp;
+
+	size = BUFSIZ * 2;
+	p = malloc(size + 1);
+	if (!p)
+		return ok(1, "%s(): malloc(%zu) failed", /* gncov */
+		             __func__, size + 1);
+
+	memset(p, 'a', size);
+	p[3] = 'b';
+	p[4] = 'c';
+	p[size] = '\0';
+
+	outp = diag_output("%s", p);
+	r += ok(!outp, "diag_big: diag_output() returns ok");
+	r += ok(!(strlen(outp) == size + 2),
+	        "diag_big: String length is correct");
+	r += ok(!!strncmp(outp, "# aaabcaaa", 10),
+	        "diag_big: Beginning is ok");
+	free(outp);
+	free(p);
+
+	return r;
+}
+
+/*
+ * test_diag() - Tests the diag_output() function. diag() can't be tested 
+ * directly because it would pollute the the test output. Returns the number of 
+ * failed tests.
+ */
+
+static int test_diag(void) {
+	int r = 0;
+	char *p, *s;
+
+	diag("Test diag()");
+
+	r += ok(!diag(NULL), "diag(NULL)");
+	r += ok(!(diag_output(NULL) == NULL), "diag_output() receives NULL");
+
+	p = diag_output("Text with\nnewline");
+	r += ok(!p, "diag_output() with newline didn't return NULL");
+	s = "# Text with\n# newline";
+	r += ok(p ? !!strcmp(p, s) : 1,
+	        "diag_output() with newline, output is ok");
+	free(p);
+
+	p = diag_output("%d = %s, %d = %s, %d = %s",
+	                1, "one", 2, "two", 3, "three");
+	r += ok(!p, "diag_output() with %%d and %%s didn't return NULL");
+	s = "# 1 = one, 2 = two, 3 = three";
+	r += ok(p ? !!strcmp(p, s) : 1, "diag_output() with %%d and %%s");
+	free(p);
+
+	r += test_diag_big();
+
+	return r;
+}
+
+/*
  * test_functions() - Tests various functions directly. Returns the number of 
  * failed tests.
  */
@@ -60,8 +249,12 @@ static int test_functions(void)
 {
 	int r = 0;
 
+	diag("Test selftest routines");
 	r += ok(!ok(0, NULL), "ok(0, NULL)");
+	r += test_diag();
 
+	diag("Test various routines");
+	diag("Test myerror()");
 	errno = EACCES;
 	r += ok(!(myerror("errno is EACCES") > 37),
 	        "myerror(): errno is EACCES");
@@ -80,7 +273,15 @@ int opt_selftest(void)
 {
 	int r = 0;
 
+	diag("Running tests for %s %s (%s)",
+	     progname, EXEC_VERSION, EXEC_DATE);
+
 	r += test_functions();
+
+	printf("1..%d\n", testnum);
+	if (r)
+		diag("Looks like you failed %d test%s of %d.", /* gncov */
+		     r, (r == 1) ? "" : "s", testnum);
 
 	return r ? EXIT_FAILURE : EXIT_SUCCESS;
 }
